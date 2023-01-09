cdfc15b13ec729e354f0467b397d51d02c3126d9
mirakov <holturaevm@gmail.com>
2023-01-09 12:40:20 +0500

    create ShoppingCartModal componet, now it is available to see the selected products

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,24 @@
+import { Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItems from "../data/items.json";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+  console.log('item is', item)
+  if (item === null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2}>
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      />
+    </Stack>
+  );
+}
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCartModal } from "../components/ShoppingCartModal";
 
 export function useShoppingCart() {
   return useContext(ShoppingCartContext);
@@ -14,16 +15,29 @@ type CartItemType = {
 };
 
 type ShoppingCartContextType = {
+  openCart: () => void;
+  closeCart: () => void;
   getItemQuantity: (id: number) => number;
   increaseCartQuantity: (id: number) => void;
   decreaseCartQuantity: (id: number) => void;
   removeFromCart: (id: number) => void;
+  cartItems: CartItemType[];
+  cartQuantity: number;
 };
 
 const ShoppingCartContext = createContext({} as ShoppingCartContextType);
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const openCart = () => setIsOpen(true);
+  const closeCart = () => setIsOpen(false);
 
   function getItemQuantity(id: number) {
     return cartItems.find((item) => item.id === id)?.quantity || 0;
@@ -74,9 +88,14 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
         increaseCartQuantity,
         decreaseCartQuantity,
         removeFromCart,
+        openCart,
+        closeCart,
+        cartItems,
+        cartQuantity,
       }}
     >
       {children}
+      <ShoppingCartModal isOpen={isOpen} cartItems={cartItems} />
     </ShoppingCartContext.Provider>
   );
 }
